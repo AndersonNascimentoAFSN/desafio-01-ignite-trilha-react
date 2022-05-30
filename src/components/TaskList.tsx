aeb598cb15356224bb8c7817cfb8dce047ad744a
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-05-30 16:29:20 -0300

    add operator ternary

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import '../styles/tasklist.scss'
+import "../styles/tasklist.scss";
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from "react-icons/fi";
 
 interface Task {
   id: number;
@@ -12,32 +12,35 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return;
     const id = Math.random();
-    setTasks((prevState) =>
-      [...prevState, { id, title: newTaskTitle, isComplete: false }]);
-    setNewTaskTitle('');
+    setTasks((prevState) => [
+      ...prevState,
+      { id, title: newTaskTitle, isComplete: false },
+    ]);
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const tasksCompletion = tasks.map((task) => {
-      if (task.id === id) {
-        task.isComplete = !task.isComplete;
-        return task;
-      }
-      return task;
-    });
+    const tasksCompletion = tasks.map((task) =>
+      task.id === id
+        ? {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+        : task
+    );
     setTasks(tasksCompletion);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    const tasksFiltered = tasks.filter((task) => task.id !== id)
+    const tasksFiltered = tasks.filter((task) => task.id !== id);
     setTasks(tasksFiltered);
   }
 
@@ -53,7 +56,11 @@ export function TaskList() {
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={handleCreateNewTask}
+          >
             <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
@@ -61,9 +68,12 @@ export function TaskList() {
 
       <main>
         <ul>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+              <div
+                className={task.isComplete ? "completed" : ""}
+                data-testid="task"
+              >
                 <label className="checkbox-container">
                   <input
                     type="checkbox"
@@ -76,14 +86,17 @@ export function TaskList() {
                 <p>{task.title}</p>
               </div>
 
-              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
+              <button
+                type="button"
+                data-testid="remove-task-button"
+                onClick={() => handleRemoveTask(task.id)}
+              >
                 <FiTrash size={16} />
               </button>
             </li>
           ))}
-
         </ul>
       </main>
     </section>
-  )
+  );
 }
